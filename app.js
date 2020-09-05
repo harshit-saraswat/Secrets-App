56a8c28c5b23678f4e7f3eaaf02a2e6a7b28c621
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-09-05 23:02:03 +0530

    Added Level 1 Authentication - Register and Login User with Email and Password

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 // App Setup
 const app = express();
@@ -9,19 +10,75 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+// DB Setup
+mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true, useNewUrlParser: true });
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "No email specified."]
+    },
+    password: {
+        type: String,
+        required: [true, "No password specified."]
+    }
+});
+
+// User Model
+const User = mongoose.model("User", userSchema);
+
 // Home Route
 app.get("/",function(req,res){
     res.render("home");
 });
 
-// Login Route
+// Login Get Route
 app.get("/login",function(req,res){
     res.render("login");
 });
 
-// Register Route
+// Login Post Route
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(!foundUser){
+                res.redirect("/login");
+            }else{
+                if(foundUser.password==password){
+                    res.render("secrets");
+                }else{
+                    res.redirect("/login");
+                }
+            }
+        }
+    });
+});
+
+// Register Get Route
 app.get("/register",function(req,res){
     res.render("register");
+});
+
+// Register Post Route
+app.post("/register",function(req,res){
+    const newUser = User({
+        email:req.body.username,
+        password:req.body.password
+    });
+
+    newUser.save(function(err){
+        if (err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
 });
 
 // App Run
